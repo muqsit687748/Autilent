f89cd8186ef6aa532d7226f449e8430e005dae2d
Muqsit687748 <muqsit.abbasi@bytecorp.io>
2025-08-02 18:34:38 +0100

    Update netlify dev configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // App directory is now stable in Next.js 14
-}
+  // Server Actions are now available by default
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
